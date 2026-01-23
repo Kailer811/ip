dbf120a54bf517763f7ea3639a14da2f9b109183
Kailer <limklethan@gmail.com>
2026-01-23 23:37:04 +0800

    Level 0: Renamed Duke to Yilonmah. Added *.class to gitignore

---
+++ src/main/java/Yilonmah.java
@@ -0,0 +1,9 @@
+public class Yilonmah {
+    public static void main(String[] args) {
+        String logo = "YiLonMah";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("");
+        System.out.println("Bye, Hope to see you again soon!");
+    }
+}
