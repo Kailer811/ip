6c096c7dbc3f70e05dc4ea258c17888238abfb97
Kailer <limklethan@gmail.com>
2026-01-26 14:40:21 +0800

    Level 2: Add, List. Completed

--- src/main/java/Yilonmah.java
+++ src/main/java/Yilonmah.java
@@ -4,6 +4,8 @@ public class Yilonmah {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String line = "";
+        String[] list = new String[100];
+        int listIdx = 0;
 
         String logo = "YiLonMah";
         System.out.println("Hello! I'm " + logo);
@@ -12,7 +14,16 @@ public class Yilonmah {
         while (!line.equals("bye")) {
             line = in.nextLine();
             if (!line.equals("bye")) {
-                System.out.println(line);
+                if (!line.equals("list")) {
+                    System.out.println("added: " + line);
+                    list[listIdx] = line;
+                    listIdx++;
+                } else {
+                    for (int i = 0; i < listIdx; i++) {
+                        int j = i + 1;
+                        System.out.println(j + ". " + list[i]);
+                    }
+                }
             } else {
                 System.out.println("Bai Baiii");
             }
