aedfbc0e05b304bcae61a3e3e312d47b654c3671
Kailer <limklethan@gmail.com>
2026-01-26 12:05:46 +0800

    Level 1: Echo completed

--- src/main/java/Yilonmah.java
+++ src/main/java/Yilonmah.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Yilonmah {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line = "";
+
         String logo = "YiLonMah";
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println("Bye, Hope to see you again soon!");
+        // System.out.println("");
+        // System.out.println("Bye, Hope to see you again soon!");
+        while (!line.equals("bye")) {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println(line);
+            } else {
+                System.out.println("Bai Baiii");
+            }
+        }
     }
 }
